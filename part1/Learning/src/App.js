e2a763c6e8ff7974df10621dff39c13835624c18
Kwagel <92309850+Kwagel@users.noreply.github.com>
2022-06-22 10:42:29 +0100

    rules of hooks complete

--- part1/Learning/src/App.js
+++ part1/Learning/src/App.js
@@ -1,26 +1,55 @@
 import {useState} from 'react'
 
-const App = () => {
-    const [counter, setCounter] = useState(0)
-
-    const increaseByOne = () => setCounter(counter + 1)
-    const decreaseByOne = () => setCounter(counter - 1)
-    const setToZero = () => setCounter(0)
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
     return (
         <div>
-
-            <Display counter={counter}/>
-            <Button onClick={increaseByOne} text='plus'/>
-            <Button onClick={decreaseByOne} text='minus'/>
-            <Button onClick={setToZero} text='zero'/>
-
-
+            button press history: {props.allClicks.join(' ')}
         </div>
     )
 }
-const Display = ({counter}) => <div>{counter}</div>
 
+const Button = (props) => {
+    console.log('props value is',props)
+    const {handleClick, text} = props
+    return (
+        < button
+            onClick={handleClick}>
+            {text}
+        </button>
+    )
+}
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const App = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
+
+    return (
+        <div>
+            {left}
+            <Button handleClick={handleLeftClick} text='left'/>
+            <Button handleClick={handleRightClick} text='right'/>
+            {right}
+            <History allClicks={allClicks}/>
+        </div>
+    )
+}
 
 export default App
